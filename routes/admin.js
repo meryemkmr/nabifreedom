04580e098c444cfb76d965ca71f930c54ee90a14
John Kearney <John.d.kearney1@gmail.com>
2020-03-24 11:57:27 -0500

    got new and old contact msgs displaying on admin page

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 let role = '';
 let loggedIn = false;
+const db = require('../models');
 
 let auth = (req, res, next) => {
       if(req.session.userid == 'admin') { //if the proper cookie exists
@@ -15,14 +16,41 @@ let auth = (req, res, next) => {
 
 //ADMIN PAGE
 router.get('/admin', auth, ((req, res) => {
-    res.render('admin',{
-        pageTitle: 'ADMIN',
-        role: role,
-        loggedIn: loggedIn
-    });
+    //get all the users first
+    db.users.findAll()
+    .then((userResults) => {
+        //get the contact table next
+        db.contact.findAll()
+        .then((contactResults) => {
+            //the the peoples table next
+            db.peoples.findAll()
+            .then((peoplesResults) => {
+                res.render('admin',{
+                    pageTitle: 'ADMIN',
+                    role: role,
+                    loggedIn: loggedIn,
+                    userResults: userResults,
+                    contactResults: contactResults,
+                    peoplesResults: peoplesResults
+                });
+            })
+        })
+    })
+    
+    
+    // res.render('admin',{
+    //     pageTitle: 'ADMIN',
+    //     role: role,
+    //     loggedIn: loggedIn
+    // });
 }))
 
 
+
+// .catch((error)=>{
+//     res.redirect('/');
+//     console.log(error);
+// })
 
 
 module.exports = router;
